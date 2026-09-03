d3ba8e01a7ca968ea246628d56617e9f086efb1c
清风 <pipelining@qq.com>
2026-09-03 20:04:11 +0800

    新增图片渐进式加载

---
+++ scripts/progressive-images.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// Keep article images as ordinary <img> elements while opting them into
+// browser-native lazy loading and asynchronous decoding.
+hexo.extend.filter.register('after_post_render', function (data) {
+  data.content = data.content.replace(/<img\b[^>]*>/gi, function (tag) {
+    if (/\b(?:no-lazy|loading|data-src)\b/i.test(tag)) return tag;
+
+    if (/\bclass\s*=/i.test(tag)) {
+      return tag
+        .replace(/\bclass\s*=\s*(["'])(.*?)\1/i, function (_, quote, classes) {
+          return 'class=' + quote + classes + ' progressive-image' + quote;
+        })
+        .replace(/^<img\b/i, '<img loading="lazy" decoding="async"');
+    }
+
+    return tag.replace(/^<img\b/i, '<img class="progressive-image" loading="lazy" decoding="async"');
+  });
+  return data;
+});
